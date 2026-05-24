9bb503a1b2229993a062adb6cf421ea5c5a4be2a
Mohammed Taysir <medo-11223344@hotmail.com>
2026-05-24 16:39:39 +0300

    products page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,27 @@
 
 const listIcon = document.querySelector(".list-icon");
 
-const modal = document.querySelector(".modal");
+const navModal = document.querySelector(".nav-modal");
 
-const modalCloseButton = document.querySelector(".modal .close-button");
+const modalCloseButton = document.querySelector(".nav-modal .close-button");
 
 modalCloseButton.addEventListener("click", () => {
-  modal.classList.remove("active");
+  navModal.classList.remove("active");
 });
 
 listIcon.addEventListener("click", () => {
-  modal.classList.add("active");
+  navModal.classList.add("active");
 });
 
+
+const filterModalButton = document.querySelector(".filter-modal-icon");
+const filterModal = document.querySelector(".filter-modal");
+const filterModalClose = document.querySelector(".filter-modal .close-button");
+
+filterModalClose.addEventListener("click", () => {
+  filterModal.classList.remove("active");
+});
+
+filterModalButton.addEventListener("click", () => {
+  filterModal.classList.add("active");
+});
